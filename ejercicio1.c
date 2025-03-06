e787479654543886831567f19418ebc10dabe8ad
BOMBO089 <gonzalezaloonso12@gmail.com>
2025-03-06 13:55:19 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,22 @@
+//Alonso Gonzalez
+//Ejercicio
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  int a=3;
+  int b=2;
+
+  if (a>b)
+  {
+    
+    printf("A es mayor \n");
+    
+  }
+
+
+  
+}
