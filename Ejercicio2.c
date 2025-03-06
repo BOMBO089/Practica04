65c8e1d018b731b61777f344e274448e66838586
BOMBO089 <gonzalezaloonso12@gmail.com>
2025-03-06 14:23:15 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,23 @@
+//Alonso Gonzalez
+//Ejercicio 2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  
+  int a=3;
+  int b=4;
+  
+  if(a>b)
+  {
+    printf("A es mayor \n");
+  }
+  else
+  {
+    printf("B es mayor o igual \n");
+  }
+  return 0;
+
+}
